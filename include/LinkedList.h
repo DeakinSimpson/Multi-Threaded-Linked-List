b91f7012bf6bd4ac3f2bc3ad3a108ba379dc06e8
DeakinSimpson <deakinsimpson976@gmail.com>
2026-09-22 15:21:14 +1000

    Add inserting to front if no head

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -6,5 +6,36 @@
 
 namespace LinkedList
 {
+    struct Node
+    {
+        int data;
+        Node* next;
 
+        /**
+         *
+         * @param data The integer that the node will contain
+         */
+        explicit Node(const int data) : data(data), next(nullptr) {  }
+    };
+
+    class LinkedList
+    {
+        Node* head_;
+
+    public:
+
+        LinkedList() : head_(nullptr) {  }
+
+        int Front() const { return head_->data; }
+
+        void InsertBeginning(const int data)
+        {
+            // if the list is empty, initialise head with data
+            if (!head_)
+            {
+                head_ = new Node(data);
+            }
+        }
+
+    };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,11 @@ int main()
 {
     std::cout << "Hello World!" << std::endl;
 
+    LinkedList::LinkedList linked_list;
+
+    linked_list.InsertBeginning(5);
+
+    std::cout << linked_list.Front() << std::endl;
+
     return 0;
 }
